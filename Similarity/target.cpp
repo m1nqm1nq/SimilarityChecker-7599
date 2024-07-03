390f15c94c3c6f8f2870540aaf98e7f00af1d9c8
minq <james2224@hanmail.net>
2024-07-03 16:42:39 +0900

    change function name

--- Similarity/target.cpp
+++ Similarity/target.cpp
@@ -6,10 +6,10 @@ class SimilarityChecker
 public:
     int Check(string A, string B)
     {
-        return Check1_LetterNum(A, B) + Check2_Alphabet(A, B);
+        return Check1_Length(A, B) + Check2_Alphabet(A, B);
     }
 
-    int Check1_LetterNum(string A, string B)
+    int Check1_Length(string A, string B)
     {
         return 0;
     }
