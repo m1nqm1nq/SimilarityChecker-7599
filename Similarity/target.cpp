a57a01266d3e7150eed58cf26ffca706caf426aa
minq <james2224@hanmail.net>
2024-07-03 17:05:34 +0900

    feature/length

--- Similarity/target.cpp
+++ Similarity/target.cpp
@@ -11,7 +11,23 @@ public:
 
     int Check1_Length(string A, string B)
     {
-        return 0;
+        int lenA = A.length();
+        int lenB = B.length();
+        if (lenA == lenB) return 60;
+        if (lenA == 0 || lenB == 0) return 0;
+
+        int result; // = (1 - (lenLong - lenShort) / lenShort) * 60
+        if (lenA >= lenB)
+        {
+            result = (lenB * 2 - lenA) * 60 / lenB;
+        }
+        else
+        {
+            result = (lenA * 2 - lenB) * 60 / lenA;
+        }
+
+        if (result < 0) return 0;
+        return result;
     }
 
     int Check2_Alphabet(string A, string B)
--- Testable/test.cpp
+++ Testable/test.cpp
@@ -4,7 +4,38 @@
 #include <string>
 using namespace std;
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(TsSimilarity, TcCheck1LengthIsEmptyBoth)
+{
+    SimilarityChecker sc;
+    EXPECT_EQ(sc.Check1_Length("", ""), 60);
+}
+
+TEST(TsSimilarity, TcCheck1LengthIsEmpty1)
+{
+    SimilarityChecker sc;
+    EXPECT_EQ(sc.Check1_Length("ABC", ""), 0);
+}
+
+TEST(TsSimilarity, TcCheck1LengthIsDiff1)
+{
+    SimilarityChecker sc;
+    EXPECT_EQ(sc.Check1_Length("ABC", "A"), 0);
+}
+
+TEST(TsSimilarity, TcCheck1LengthIsDiff2)
+{
+    SimilarityChecker sc;
+    EXPECT_EQ(sc.Check1_Length("ABC", "AB"), 30);
+}
+
+TEST(TsSimilarity, TcCheck1LengthIsDiff3)
+{
+    SimilarityChecker sc;
+    EXPECT_EQ(sc.Check1_Length("ABC", "ABCDE"), 20);
+}
+
+TEST(TsSimilarity, TcCheck1LengthIsDiff4)
+{
+    SimilarityChecker sc;
+    EXPECT_EQ(sc.Check1_Length("ABCEFGHIJKLMN", "ABCDEFGHIJKL"), 55);
 }
